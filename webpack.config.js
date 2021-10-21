436e7b5c4243221317600c0ba82b37f297b82048
katerina2018 <tvorchestvo15@gmail.com>
2021-10-21 15:09:08 +0300

    commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+    },
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
+    },
+};
